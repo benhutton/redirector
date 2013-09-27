4c60cc4ff61d0b4348375cc7f85866d0396efcf6
Ben Hutton <benhutton@gmail.com>
2013-09-27 17:41:27 -0400

    Lets manually configure this

--- lib/redirector/engine.rb
+++ lib/redirector/engine.rb
@@ -2,9 +2,9 @@ module Redirector
   class Engine < ::Rails::Engine
     config.redirector = ActiveSupport::OrderedOptions.new
 
-    initializer "redirector.add_middleware" do |app|
-      app.middleware.insert_before(Rack::Lock, Redirector::Middleware)
-    end
+    # initializer "redirector.add_middleware" do |app|
+      # app.middleware.insert_before(Rack::Lock, Redirector::Middleware)
+    # end
 
     initializer "redirector.apply_options" do |app|
       Redirector.include_query_in_source = app.config.redirector.include_query_in_source || false
